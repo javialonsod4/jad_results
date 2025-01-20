32240945f5bc6298bcbcf3278f6ecc9c1e5e8564
javialonsod4 <166646496+javialonsod4@users.noreply.github.com>
2025-01-20 12:50:07 +0100

    Update proyecto1.py

--- proyecto1.py
+++ proyecto1.py
@@ -1,20 +1,33 @@
 import pandas as pd
+import mysql.connector
 
-import boto3
+# Conectar a la base de datos RDS
+mydb = mysql.connector.connect(
+  host="tu_host_rds",
+  user="tu_usuario",
+  password="tu_password",
+  database="tu_base_de_datos"
+)
 
-s3 = boto3.client('s3')
+# Leer el archivo CSV desde S3 (ajusta la ruta y el bucket)
+df = pd.read_csv('s3://mi-bucket/resultados_futbol.csv')
 
-bucket_name = 'jad-resultados-big-data'
-object_key = 'results.csv'
+# Crear la tabla si no existe
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE IF NOT EXISTS victorias (equipo VARCHAR(255), victorias INT)")
 
-# Descargar el archivo a un objeto en memoria
-response = s3.get_object(Bucket=bucket_name, Key=object_key)
-data = response['Body'].read().decode('utf-8')
+# Insertar los datos en la tabla
+for index, row in df.iterrows():
+    equipo = row['equipo']
+    victorias = row['victorias']
+    sql = "INSERT INTO victorias (equipo, victorias) VALUES (%s, %s)"
+    val = (equipo, victorias)
+    mycursor.execute(sql, val)
 
-# Crear un DataFrame de pandas a partir del contenido
-df = pd.read_csv(io.StringIO(data))
+mydb.commit()
+print(mycursor.rowcount, "record inserted.")
 
-
+'''
 #data = pd.read_csv('data/results.csv', sep=',')
 print('Primeras filas:\n', df.head())
 print('\nNombre de las columnas:\n', df.columns)
@@ -45,3 +58,4 @@ df['winner'] = winner_list
 df['loser'] = loser_list
 
 print('\nPrimeras filas:\n', df.head())
+'''
