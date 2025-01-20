47b7113a4a62f18160de238da63ad41cdef20c3b
javialonsod4 <166646496+javialonsod4@users.noreply.github.com>
2025-01-20 14:26:53 +0100

    Update proyecto3.py

--- proyecto3.py
+++ proyecto3.py
@@ -4,13 +4,13 @@ session = Session()
 
 def down(bucket_name, bucket_path, dest_path):
     s3 = session.resource('s3')
-    bucket = s3.bucket(bucket_name)
+    bucket = s3.Bucket(bucket_name)
     bucket.download_file(bucket_path, dest_path)
 
     print(f'{bucket_name} downloaded in {dest_path}')
 
 if __name__ == '__main__':
-    bucket_name = 'resultsdb'
+    bucket_name = 'jad-f-results-bucket'
     bucket_path = 'results.csv'
     dest_path = 'data'
     down(bucket_name, bucket_path, dest_path)
