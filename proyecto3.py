88f0702f6bfa492d3faf45f29a1be631c8301c11
javialonsod4 <166646496+javialonsod4@users.noreply.github.com>
2025-01-20 13:24:28 +0100

    Add files via upload

---
+++ proyecto3.py
@@ -0,0 +1,16 @@
+from boto3 import Session
+
+session = Session()
+
+def down(bucket_name, bucket_path, dest_path):
+    s3 = session.resource('s3')
+    bucket = s3.bucket(bucket_name)
+    bucket.download_file(bucket_path, dest_path)
+
+    print(f'{bucket_name} downloaded in {dest_path}')
+
+if __name__ == '__main__':
+    bucket_name = 'resultsdb'
+    bucket_path = 'results.csv'
+    dest_path = 'data'
+    down(bucket_name, bucket_path, dest_path)
